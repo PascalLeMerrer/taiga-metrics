801c452ef873f99abed803557c23d3676e5bad78
Pascal Le Merrer <pascal.lemerrer@orange.com>
2017-10-29 15:03:16 +0100

    Move ProjectConfig model to models file

--- server/app.py
+++ server/app.py
@@ -23,24 +23,7 @@ APP.debug = True
 # initialize the database connection
 DB = SQLAlchemy(APP)
 
-class ProjectConfig(DB.Model):
-    """Database model for project preferences about metrics."""
-
-    __tablename__ = 'project_config'
-    project_id = DB.Column(DB.Integer, primary_key=True)
-    work_start_status = DB.Column(DB.Integer, default=0)
-    finish_status = DB.Column(DB.Integer, default=0)
-
-    def __repr__(self):
-        return "<ProjectConfig {}: start status= {}, end status={}".format(
-            self.project_id, self.work_start_status, self.finish_status)
-
-    def __init__(self, project_id=None,
-                 work_start_status=None,
-                finish_status=None):
-        self.project_id = project_id
-        self.work_start_status = work_start_status
-        self.finish_status = finish_status
+from models import ProjectConfig
 
 
 # initialize database migration management
