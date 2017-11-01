00067e4be6ce05bb5c2f3908ef72e835a1cd2ec4
Pascal Le Merrer <pascal.lemerrer@orange.com>
2017-11-01 16:31:51 +0100

    Fix behave parameter passing

--- feature-tests/tests.sh
+++ feature-tests/tests.sh
@@ -3,6 +3,8 @@
 # This scripts waits for the API server to be ready before running functional tests with Behave
 # when invoked with parameters, they are used as tagged to be passed to behave
 # It allows running tests with specific tags
+
+
 until curl --location --silent "http://server:5000/isalive" > /dev/null 2>&1; do
   >&2 echo "server is not ready - sleeping"
   sleep 1
@@ -13,7 +15,7 @@ done
 tags="--tags=-wip"
 if [ $1 ]
 then
- tags+=" --tags="
+ tags=" --tags="
  tags+=$1
 fi
 
