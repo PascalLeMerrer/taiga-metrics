9170c02a9975e712ce96aae9a360eecde137b4ca
Pascal Le Merrer <pascal.lemerrer@gmail.com>
2017-12-16 12:34:43 +0100

    Refactor auth

--- server/auth.py
+++ server/auth.py
@@ -3,52 +3,53 @@ from functools import wraps
 
 import requests
 import os
+import logging
 import json
 import sys
 
+logger = logging.getLogger('auth')
+
 AUTH_HEADER = "Authorization"
 API_URL = os.getenv("TAIGA_API_URL", 'https://api.taiga.io/api/v1')
-print(f"API_URL: {API_URL}")
-sys.stdout.flush()
 
 def are_valid_credentials(data):
     return False
 
 
 def authenticate(data):
-    print(f'authenticate called with data {data}\n\n')
-    sys.stdout.flush()
-    if data is None:
-        return False
-    if 'username' not in data:
-        return False
-    if 'password' not in data:
+
+    if data is None or 'username' not in data or 'password' not in data:
         return False
 
+    user_profile = _delegate_authentication(data['username'], data['password'])
+    if not user_profile:
+        return False
+
+    return {
+          "username": user_profile['username'],
+          "full_display_name": user_profile['full_name_display'],
+          "auth_token": user_profile['auth_token']
+    }
+
+
+def _delegate_authentication(username, password):
+    """ makes a request to Taiga to authenticate the user with the provided credentials"""
     payload = json.dumps({
         'type': 'normal',
-        'username': data['username'],
-        'password': data['password']
+        'username': username,
+        'password': password
     })
     headers = {'Content-Type': 'application/json'}
-
-    print(f'Ready to send POST to {API_URL + "/auth"} with payload {payload} and headers {headers}')
-    sys.stdout.flush()
-
-
     login_response = requests.post(API_URL + "/auth", data=payload, headers=headers)
-    print(f'login_response: {login_response}')
-    sys.stdout.flush()
     if login_response.status_code != 200:
         return False
 
-    # TODO catch decoding exception
-    decoded_response = login_response.json()
-    return {
-          "username": decoded_response['username'],
-          "full_display_name": decoded_response['full_name_display'],
-          "auth_token": decoded_response['auth_token']
-    }
+    try:
+        decoded_response = login_response.json()
+    except ValueError as error:
+        logger.error(f'Cannot decode Taiga auth response: {error}. Response was: {login_response}')
+        return False
+    return decoded_response
 
 
 def requires_authentication(f):
